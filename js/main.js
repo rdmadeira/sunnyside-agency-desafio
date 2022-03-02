f3a5f3ad607710d81d2a578275a13eb5d87983b6
rdmadeira <rdmadeira2@gmail.com>
2022-03-02 16:56:13 -0300

    construyendo aside y footer

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* const links = document.querySelectorAll(".creatives picture a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+} 
+ */
+/* const links = document.querySelectorAll(".sieta");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+ 
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth"
+  });
+} */
